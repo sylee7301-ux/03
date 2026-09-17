ed5e9fbefc545c5eaa5355abc822c9541907e089
Lee siyeon <sylee7301@sookmyung.ac.kr>
2026-09-17 16:28:22 +0900

    03

--- main.c
+++ main.c
@@ -2,13 +2,11 @@
 
 int main(void) {
     char c;
-    int i;
 
-    printf("input a number :");
+    printf("enter a character: ");
     scanf("%c", &c);
 
-    i=c-'0';
-    printf("The input number is %i\n", i);
+    printf("the next character of %c (%i) is %c (%i)\n", c, c, c+1, c+1);
 
     return 0;
 }
